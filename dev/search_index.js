f5b2ecf2379225fc2c63cb62939e36dd2b8dc1df
Documenter.jl <documenter@juliadocs.github.io>
2025-08-12 06:50:46 +0000

    build based on b56faea

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Cartographer","page":"Home","title":"Cartographer","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Cartographer.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"#Cartographer.nsimilar-Tuple{Type, Type, AbstractArray{<:AbstractArray}}","page":"Home","title":"Cartographer.nsimilar","text":"Construct a similar nested array to x with new leaves of type outleaf, for original leaves of type inleaf\n\n\n\n\n\n","category":"method"}]
+}
