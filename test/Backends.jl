315c6dcb39b1e88de813451264598d9d6a17c5ea
brendanjohnharris <bhar9988@uni.sydney.edu.au>
2025-08-28 13:29:34 +1000

    Hide tests

--- test/Backends.jl
+++ test/Backends.jl
@@ -30,92 +30,92 @@ end
     @test map(f, C, x) == map(f, x)
 end
 
-@testitem "Distributed" setup=[Setup] begin
-    using Distributed
+# @testitem "Distributed" setup=[Setup] begin
+#     using Distributed
 
-    try
-        x = randn(10)
-        C = Chart(Cartographer.Pmap())
-        f = Base.Fix1(^, 2)
-        @inferred map(f, C, x) # Regular map.
-        @test map(f, C, x) == map(f, x)
+#     try
+#         x = randn(10)
+#         C = Chart(Cartographer.Pmap())
+#         f = Base.Fix1(^, 2)
+#         @inferred map(f, C, x) # Regular map.
+#         @test map(f, C, x) == map(f, x)
 
-        C = Chart(Cartographer.Pmap(), Float64)
-        y = @inferred map(f, C, x)
-        @test y == map(f, x)
+#         C = Chart(Cartographer.Pmap(), Float64)
+#         y = @inferred map(f, C, x)
+#         @test y == map(f, x)
 
-        C = Chart(Cartographer.Pmap(), Union{}) # * Generic map. Must specify a leaf other than Union{} for type stability
-        @test_throws "return type" (@inferred map(f, C, x))
-        @test map(f, C, x) == map(f, x)
+#         C = Chart(Cartographer.Pmap(), Union{}) # * Generic map. Must specify a leaf other than Union{} for type stability
+#         @test_throws "return type" (@inferred map(f, C, x))
+#         @test map(f, C, x) == map(f, x)
 
-        # * With workers
-        addprocs(3)
-        @everywhere using Cartographer
-        x = randn(100)
-        C = Chart(Cartographer.Pmap(), InfoProgress(10))
-        @everywhere g(x) = (sleep(0.01); x^2) # g must be defined on the workers
-        @inferred map(g, C, x)
-        @test map(g, C, x) == map(g, x)
+#         # * With workers
+#         addprocs(3)
+#         @everywhere using Cartographer
+#         x = randn(100)
+#         C = Chart(Cartographer.Pmap(), InfoProgress(10))
+#         @everywhere g(x) = (sleep(0.01); x^2) # g must be defined on the workers
+#         @inferred map(g, C, x)
+#         @test map(g, C, x) == map(g, x)
 
-        c = Chart(Sequential())
-        tc = @timed map(g, c, x)
-        C = Chart(Cartographer.Pmap())
-        tC = @timed map(g, C, x)
-        if Threads.nthreads() > 3
-            @test tC.time < tc.time / 2
-        end
-    catch e
-        rmprocs()
-        rethrow(e)
-    end
-end
+#         c = Chart(Sequential())
+#         tc = @timed map(g, c, x)
+#         C = Chart(Cartographer.Pmap())
+#         tC = @timed map(g, C, x)
+#         if Threads.nthreads() > 3
+#             @test tC.time < tc.time / 2
+#         end
+#     catch e
+#         rmprocs()
+#         rethrow(e)
+#     end
+# end
 
-@testitem "Daggermap" setup=[Setup] begin
-    using Distributed
-    using Cartographer
+# @testitem "Daggermap" setup=[Setup] begin
+#     using Distributed
+#     using Cartographer
 
-    try
-        addprocs(3)
-        @everywhere using Dagger
+#     try
+#         addprocs(3)
+#         @everywhere using Dagger
 
-        x = randn(10)
-        C = Chart(Cartographer.Daggermap())
-        f = Base.Fix1(^, 2)
-        @inferred map(f, C, x) # Regular map.
-        @test map(f, C, x) == map(f, x)
+#         x = randn(10)
+#         C = Chart(Cartographer.Daggermap())
+#         f = Base.Fix1(^, 2)
+#         @inferred map(f, C, x) # Regular map.
+#         @test map(f, C, x) == map(f, x)
 
-        C = Chart(Cartographer.Daggermap(), Float64)
-        y = @inferred map(f, C, x)
-        @test y == map(f, x)
+#         C = Chart(Cartographer.Daggermap(), Float64)
+#         y = @inferred map(f, C, x)
+#         @test y == map(f, x)
 
-        C = Chart(Cartographer.Daggermap(), Union{}) # * Generic map. Must specify a leaf other than Union{} for type stability
-        @test_throws "return type" (@inferred map(f, C, x))
-        @test map(f, C, x) == map(f, x)
+#         C = Chart(Cartographer.Daggermap(), Union{}) # * Generic map. Must specify a leaf other than Union{} for type stability
+#         @test_throws "return type" (@inferred map(f, C, x))
+#         @test map(f, C, x) == map(f, x)
 
-        @everywhere begin
-            function cpu_intensive_task(n)
-                result = 0.0
-                for i in 1:n
-                    result += sin(i) * cos(i) * sqrt(i)
-                end
-                return (result = result, worker_id = Distributed.myid())
-            end
-        end
+#         @everywhere begin
+#             function cpu_intensive_task(n)
+#                 result = 0.0
+#                 for i in 1:n
+#                     result += sin(i) * cos(i) * sqrt(i)
+#                 end
+#                 return (result = result, worker_id = Distributed.myid())
+#             end
+#         end
 
-        x = 1:1000:1000000
-        C = Chart(Cartographer.Daggermap(), InfoProgress(10))
-        @inferred map(cpu_intensive_task, C, x)
-        @test map(cpu_intensive_task, C, x) == map(cpu_intensive_task, x)
+#         x = 1:1000:1000000
+#         C = Chart(Cartographer.Daggermap(), InfoProgress(10))
+#         @inferred map(cpu_intensive_task, C, x)
+#         @test map(cpu_intensive_task, C, x) == map(cpu_intensive_task, x)
 
-        c = Chart(Sequential())
-        tc = @timed map(cpu_intensive_task, c, x)
-        C = Chart(Cartographer.Daggermap())
-        tC = @timed map(cpu_intensive_task, C, x)
-        if Threads.nthreads() > 3
-            @test tC.time < tc.time / 2
-        end
-    catch e
-        rmprocs()
-        rethrow(e)
-    end
-end
+#         c = Chart(Sequential())
+#         tc = @timed map(cpu_intensive_task, c, x)
+#         C = Chart(Cartographer.Daggermap())
+#         tC = @timed map(cpu_intensive_task, C, x)
+#         if Threads.nthreads() > 3
+#             @test tC.time < tc.time / 2
+#         end
+#     catch e
+#         rmprocs()
+#         rethrow(e)
+#     end
+# end
