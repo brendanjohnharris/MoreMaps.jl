d66a25f562e419b9a48b93637d7b154081f7e623
brendanjohnharris <bhar9988@uni.sydney.edu.au>
2025-09-03 16:04:17 +1000

    Tweak

--- ext/DaggerExt.jl
+++ ext/DaggerExt.jl
@@ -1,5 +1,10 @@
 module DaggerExt
 
+using Distributed
+import MoreMaps: Daggermap, init_log!, log_log!, close_log!, Chart, backend,
+                 preallocate, nviews
+using Dagger
+
 """
     Daggermap(; kwargs...)
 
@@ -55,12 +60,8 @@ The `options` keyword arguments are passed to `Dagger.@spawn`.
 
 See also: [`Sequential`](@ref), [`Threaded`](@ref), [`Pmap`](@ref), [`Chart`](@ref), [`map`](@ref)
 """
-MoreMaps.Daggermap
+Daggermap
 
-using Distributed
-import MoreMaps: Daggermap, init_log!, log_log!, close_log!, Chart, backend,
-                 preallocate, nviews
-using Dagger
 const DaggermapChart = Chart{L, B} where {L, B <: Daggermap}
 
 function Base.map(f, C::DaggermapChart, itrs...)
