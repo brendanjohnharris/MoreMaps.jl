76ce971d9706eae6416f67c813004358d2214f23
brendanjohnharris <bhar9988@uni.sydney.edu.au>
2025-09-11 14:50:42 +1000

    Switch to newer ProgressLogging approach

--- ext/ProgressLoggingExt.jl
+++ ext/ProgressLoggingExt.jl
@@ -80,14 +80,16 @@ function init_log!(P::MoreMaps.ProgressLogger, total)
         Threads.lock(P.info.lck) do
             Threads.atomic_add!(P.info.current, 1)
             progress = P.info.current[] * every / P.info.total
-            @logmsg PLG.ProgressLevel P.Progress.name progress _id=P.Progress.id
+            @logmsg PLG.ProgressLevel Progress(P.Progress.id, progress;
+                                               name = P.Progress.name)
         end
     end
 end
 log_log!(P::MoreMaps.ProgressLogger, i) = log_log!(P.info, i)
 
 function close_log!(P::MoreMaps.ProgressLogger)
-    @logmsg PLG.ProgressLevel progress="done" _id=P.Progress.id
+    close_log!(P.info)
+    @logmsg PLG.ProgressLevel Progress(P.Progress.id; name = P.Progress.name, done = true)
 end
 
 end
